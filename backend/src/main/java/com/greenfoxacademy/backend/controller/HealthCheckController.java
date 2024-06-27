f3789f8d82bb10f265e25684cca334d8bab89128
AnnaUgrai <149159395+AnnaUgrai@users.noreply.github.com>
2024-06-27 21:54:07 +0200

    style: move static imports above, add Javadoc

--- backend/src/main/java/com/greenfoxacademy/backend/controller/HealthCheckController.java
+++ backend/src/main/java/com/greenfoxacademy/backend/controller/HealthCheckController.java
@@ -6,6 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Endpoint created to see if app works
+ */
 @Controller
 public class HealthCheckController {
   @GetMapping ("/health-check")

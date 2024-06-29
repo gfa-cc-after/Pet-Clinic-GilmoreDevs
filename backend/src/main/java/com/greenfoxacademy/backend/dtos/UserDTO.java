3b0258b52e99542aaa93ba99ea60cc760aaeec77
AnnaUgrai <149159395+AnnaUgrai@users.noreply.github.com>
2024-06-29 21:45:45 +0200

    chore: create DTO for user entity

---
+++ backend/src/main/java/com/greenfoxacademy/backend/dtos/UserDTO.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy.backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Data Transfer Object for User Entity.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String password;
+}

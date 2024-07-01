5450ff10ccdae617872e3e2dd7692f6727651b14
Ramon Trekovanicz <ramesz6@me.com>
2024-07-01 18:22:17 +0200

    fix: indentations fix, javadoc add

--- backend/src/main/java/com/greenfoxacademy/backend/services/UserService.java
+++ backend/src/main/java/com/greenfoxacademy/backend/services/UserService.java
@@ -3,8 +3,11 @@ package com.greenfoxacademy.backend.services;
 import com.greenfoxacademy.backend.models.User;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service to manage {@link User} related actions.
+ */
 @Service
 public interface UserService {
-    void register(User newUser);
+  void register(User newUser);
 
 }
--- backend/src/main/java/com/greenfoxacademy/backend/services/UserServiceImpl.java
+++ backend/src/main/java/com/greenfoxacademy/backend/services/UserServiceImpl.java
@@ -5,13 +5,16 @@ import com.greenfoxacademy.backend.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service implementation to manage {@link UserService}.
+ */
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-    private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
-    @Override
-    public void register(User newUser) {
-        userRepository.save(newUser);
-    }
+  @Override
+  public void register(User newUser) {
+    userRepository.save(newUser);
+  }
 }

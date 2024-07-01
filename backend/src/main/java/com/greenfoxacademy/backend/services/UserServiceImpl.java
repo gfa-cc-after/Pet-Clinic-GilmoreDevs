ff6ba4d71ee2bfaa39ab63dede83b6c449dbb606
AnnaUgrai <149159395+AnnaUgrai@users.noreply.github.com>
2024-07-01 21:46:45 +0200

    chore: map RegisterUserDTO to User

--- backend/src/main/java/com/greenfoxacademy/backend/services/UserServiceImpl.java
+++ backend/src/main/java/com/greenfoxacademy/backend/services/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.greenfoxacademy.backend.services;
 
+import com.greenfoxacademy.backend.dtos.RegisterUserDTO;
 import com.greenfoxacademy.backend.models.User;
 import com.greenfoxacademy.backend.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,9 +14,22 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
   private final UserRepository userRepository;
-
+  private ModelMapper modelMapper;
+  
   @Override
   public void register(User newUser) {
     userRepository.save(newUser);
   }
+  
+  private RegisterUserDTO mapToDTO(User user) {
+    RegisterUserDTO registerUserDTO = modelMapper.map(user, RegisterUserDTO.class);
+    return registerUserDTO;
+  }
+  
+  private User mapToEntity(RegisterUserDTO userDto) {
+    User user = modelMapper.map(userDto, User.class);
+    return user;
+  }
 }
+
+

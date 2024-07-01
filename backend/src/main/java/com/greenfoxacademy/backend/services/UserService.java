da90dbec2b340e8f2b54639fcc9f94916d456a27
Ramon Trekovanicz <ramesz6@me.com>
2024-07-01 16:49:40 +0200

    fix: empty lines remove, userService now use private final repository

--- backend/src/main/java/com/greenfoxacademy/backend/services/UserService.java
+++ backend/src/main/java/com/greenfoxacademy/backend/services/UserService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
-    public void register(User newUser);
+    void register(User newUser);
 
 }
--- backend/src/main/java/com/greenfoxacademy/backend/services/UserServiceImpl.java
+++ backend/src/main/java/com/greenfoxacademy/backend/services/UserServiceImpl.java
@@ -2,12 +2,13 @@ package com.greenfoxacademy.backend.services;
 
 import com.greenfoxacademy.backend.models.User;
 import com.greenfoxacademy.backend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-    UserRepository userRepository;
-
+    private final UserRepository userRepository;
 
     @Override
     public void register(User newUser) {

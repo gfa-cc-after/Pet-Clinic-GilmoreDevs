f4b8d093cc5bbe7d882fd8f7b6ae2a764451ac45
AnnaUgrai <149159395+AnnaUgrai@users.noreply.github.com>
2024-07-01 22:00:12 +0200

    chore: add javadoc for model mapper configuration

--- backend/src/main/java/com/greenfoxacademy/backend/config/ModelMapperConfig.java
+++ backend/src/main/java/com/greenfoxacademy/backend/config/ModelMapperConfig.java
@@ -4,6 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class for setting up application-wide beans.
+ */
 @Configuration
 public class ModelMapperConfig {
   @Bean

910a2182e31ddfc8f2248e5340b555fcd718ad71
Hsbalazs <hoffartscharfi@t-online.hu>
2024-07-02 19:36:02 +0200

    refactor: changes to be compilant with reviewdog code styling

--- backend/src/main/java/com/greenfoxacademy/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/greenfoxacademy/backend/config/SecurityConfig.java
@@ -1,17 +1,24 @@
 package com.greenfoxacademy.backend.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
+/**
+ * To give security permission to all endpoints
+ */
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
+  /**
+   * To give security permission to all endpoints
+   */
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     // @formatter:off

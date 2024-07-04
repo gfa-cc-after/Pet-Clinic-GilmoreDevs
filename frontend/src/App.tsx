1a25e06f11a0494b2014ccdd2af9b5daea404663
Carlos Ponton <carlosponton20@gmail.com>
2024-07-04 16:36:15 +0200

    feat: add React FormEventHandler and ChangeEventHandler to fetch input data from the register form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import {
   RouterProvider,
   Link,
 } from "react-router-dom";
+import React, {useState} from "react";
 
 function Login() {
 
@@ -27,18 +28,53 @@ function Login() {
 }
 
 function Register() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>)=> {
+    e.preventDefault()
+    console.log('firstName:',firstName);
+    console.log('lastName:',lastName);
+    console.log('email:',email);
+    console.log('password:',password);
+  }
+
+  const saveFormData = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'firstName':
+        setFirstName(value);
+        break;
+      case 'lastName':
+        setLastName(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+  }
+
+
   return (
     <>
       <h1>Register</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>First Name:</label>
-        <input type='text'></input>
+        <input type='text' name={"firstName"} value={firstName} onChange={saveFormData}></input>
         <label>Last Name:</label>
-        <input type='text'></input>
+        <input type='text' name={"lastName"} value={lastName} onChange={saveFormData}></input>
         <label>Email:</label>
-        <input type='email'></input>
+        <input type='email' name={"email"} value={email} onChange={saveFormData}></input>
         <label>Password:</label>
-        <input type='password'></input>
+        <input type='password' name={"password"} value={password} onChange={saveFormData}></input>
         <button type='submit'>Register</button>
       </form>
       <Link className={"links"} to='/login'>Login</Link>

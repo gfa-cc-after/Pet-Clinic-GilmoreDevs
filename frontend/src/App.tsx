d5a06bd95d6344241d881230853812b09b4cea80
Carlos Ponton <carlosponton20@gmail.com>
2024-07-04 17:23:06 +0200

    feat: add fetch function to send the register form data to the backend register endpoint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -33,13 +33,24 @@ function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>)=> {
     e.preventDefault()
     console.log('firstName:',firstName);
     console.log('lastName:',lastName);
     console.log('email:',email);
     console.log('password:',password);
+    fetch('http://localhost:5180/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+      })
+    })
   }
 
   const saveFormData = (e: React.ChangeEvent<HTMLInputElement>) => {

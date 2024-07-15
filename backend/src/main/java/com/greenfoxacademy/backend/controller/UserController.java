38b1dd20f951c5eca47d4b063b8712f1f733c280
Ramon Trekovanicz <ramesz6@me.com>
2024-07-15 17:23:45 +0200

    fix: watchdog styling fix

--- backend/src/main/java/com/greenfoxacademy/backend/controller/UserController.java
+++ backend/src/main/java/com/greenfoxacademy/backend/controller/UserController.java
@@ -3,9 +3,11 @@ package com.greenfoxacademy.backend.controller;
 import com.greenfoxacademy.backend.dtos.LoginUserDto;
 import com.greenfoxacademy.backend.dtos.RegisterUserDto;
 import com.greenfoxacademy.backend.services.UserService;
+
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,11 +35,10 @@ public class UserController {
    * This method registers a new user.
    *
    * @param registerUserDto the user to be registered
-   *
    * @return a response entity with the status code and the location of the new user
    */
-  @CrossOrigin (origins = "http://localhost:5173")
-  @PostMapping ("/register")
+  @CrossOrigin(origins = "http://localhost:5173")
+  @PostMapping("/register")
   public ResponseEntity<?> registerUser(@Validated @RequestBody RegisterUserDto registerUserDto) {
     try {
       URI uri = URI.create("/users/" + userService.register(registerUserDto).id());
@@ -51,11 +52,10 @@ public class UserController {
    * This method handles validation exceptions.
    *
    * @param ex the exception to be handled
-   *
    * @return a map with the field name and the error message
    */
-  @ResponseStatus (HttpStatus.BAD_REQUEST)
-  @ExceptionHandler (MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
   public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
     Map<String, String> errors = new HashMap<>();
     ex.getBindingResult().getAllErrors().forEach((error) -> {
@@ -70,16 +70,15 @@ public class UserController {
    * This method login an existing user.
    *
    * @param loginUserDto the user to be logged in
-   *
    * @return a response entity
    */
-  @CrossOrigin (origins = "http://localhost:5173")
-  @PostMapping ("/login")
+  @CrossOrigin(origins = "http://localhost:5173")
+  @PostMapping("/login")
   public ResponseEntity<?> loginUser(@RequestBody LoginUserDto loginUserDto) {
-    Map<String,String> response = new HashMap<>();
-    response.put("Response","Login OK");
+    Map<String, String> response = new HashMap<>();
+    response.put("Response", "Login OK");
     return ResponseEntity.ok().body(response);
   }
-  
+
 }
 

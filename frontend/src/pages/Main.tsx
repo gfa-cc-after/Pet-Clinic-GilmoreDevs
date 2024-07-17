bd36677e5278fe34733d66dcb3dd907bf55aea18
Hsbalazs <hoffartscharfi@t-online.hu>
2024-07-17 21:54:52 +0200

    chore: separate main page in pages folder

---
+++ frontend/src/pages/Main.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export function Main() {
+
+    return (
+        <>
+            <h1>Home</h1>
+            <p>Welcome to Gilmore Devs Pet Clinic!</p>
+            <Link className={"links"} to='/login'>Login</Link>
+            <Link className={"links"} to='/register'>Register</Link>
+        </>
+);
+}

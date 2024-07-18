f8fe39eeb45b27ae053a541ef1b6643f7e718b41
ramesz6 <146347142+ramesz6@users.noreply.github.com>
2024-07-18 13:36:32 -0400

    Update Login.tsx remove useNaviget

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export function Login() {
     const [email, setEmail] = useState("");

db9ad5aa7640f30c92657139eeec0c5fde57f382
markkovari <kovarimarkofficial@gmail.com>
2024-07-20 15:45:41 +0200

    chore: move links to navigation

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,12 +1,15 @@
-import { PropsWithChildren } from 'react'
+import { Flex, Spacer } from '@chakra-ui/react'
 import { Link, Outlet } from 'react-router-dom';
 
 
 const Navigation = () => {
-    return <>
+    return <Flex >
+        <Link className={"links"} to='/'>Home</Link>
+        <Spacer />
         <Link className={"links"} to='/login'>Login</Link>
+        <Spacer />
         <Link className={"links"} to='/register'>Register</Link>
-    </>
+    </Flex>
 }
 
 

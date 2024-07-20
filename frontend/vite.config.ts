0c68ab85da624c8f924df51bac400fef90e04415
Ramon Trekovanicz <ramesz6@me.com>
2024-07-20 10:07:54 +0200

    Itt meg minden ok volt

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,23 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     globals: true,
     include: ['**/*.test.tsx'],
     environment: 'jsdom',
     setupFiles: ['./setup-tests.ts'],
     css: true,
+    
   }
 })

c12c3d196516678ab98cbbe014592c6e60da69d8
Hsbalazs <hoffartscharfi@t-online.hu>
2024-08-04 18:04:21 +0200

    chore: add jwt-decode dependency

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,84 +1,119 @@
 import {useNavigate} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import validator from "validator";
 import axios from "axios";
+import {jwtDecode} from "jwt-decode";
+import {Icon} from 'react-icons-kit';
+import {eyeOff} from 'react-icons-kit/feather/eyeOff';
+import {eye} from 'react-icons-kit/feather/eye'
+
+type User = { firstName: string; lastName: string; sub: string }
+
+const decodeToken = (): User | null => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        const decodeToken = jwtDecode<User>(token);
+        console.log(decodeToken);
+        return decodeToken;
+    }
+    return null;
+};
 
 export function Profile() {
-    const [email, setEmail] = useState("");
+    const userFromToken = decodeToken();
     const [password, setPassword] = useState("");
-    const [errMessage, setErrMessage] = useState<string|null>(null);
-    const [message, setMessage] = useState<string|null>(null);
-    const [token, setToken] = useState([]);
+    const [user, setUser] = useState<User | null>(userFromToken);
+    const [errMessage, setErrMessage] = useState<string | null>(null);
+    const [message, setMessage] = useState<string | null>(null);
+    const [type, setType] = useState<"password" | "text">('password');
+    const [icon, setIcon] = useState(eyeOff);
     const navigate = useNavigate();
-    const routChange = () =>{
+
+    const handleUserChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setUser({...user!, [name]: value})
+    }
+    const routChange = () => {
         const path = '/';
         navigate(path);
     }
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            setToken(token);
-        }
-    }, []);
-
-    const handleProfile = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if (validator.isEmail(email)) {
-            setErrMessage("");
-            axios.post('http://localhost:8080/profile', {
-                email,
-                password,
-            })
-                .then((_response) => {
-                    setMessage("Successful profile change!");
-                    setEmail("");
-                    setPassword("");
-                })
-                .catch((error) => {
-                    setErrMessage(error.response.data.error);
-                });
-        } else if (!validator.isEmail(email)) {
-            setErrMessage("Please, enter valid email!");
+    const handleToggle = () => {
+        if (type === 'password') {
+            setIcon(eye);
+            setType('text')
+        } else {
+            setIcon(eyeOff)
+            setType('password')
         }
     }
 
-    const saveFormData = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target;
-        switch (name) {
-            case 'email':
-                setEmail(value);
-                break;
-            case 'password':
-                setPassword(value);
-                break;
-            default:
-                break;
+    const handleProfile = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (user == null) {
+            return;
         }
-    };
+        if (!validator.isEmail(user.sub)) {
+            setErrMessage("Please, enter valid email!");
+            return;
+        }
+        setErrMessage("");
+        axios.post('http://localhost:8080/profile', {
+            email: user?.sub,
+            password,
+        })
+            .then((_response) => {
+                setMessage("Successful profile change!");
+                setPassword("");
+            })
+            .catch((error) => {
+                setErrMessage(error.response.data.error);
+            });
+    }
 
     return (
         <>
-            <h1>Profile</h1>
+            <h1>Profile update:</h1>
             <form onSubmit={handleProfile}>
                 <label htmlFor="email">Email:</label>
                 <input
                     type="email"
-                    name="email"
-                    id="email"
-                    defaultValue={token}
-                    onChange={saveFormData}
-                    required
+                    name="sub"
+                    id="sub"
+                    value={user?.sub}
+                    onChange={handleUserChange}
+                />
+                <label htmlFor="firstName">FirstName:</label>
+                <input
+                    type="text"
+                    name="firstName"
+                    id="firstName"
+                    value={user?.firstName}
+                    onChange={handleUserChange}
+                />
+                <label htmlFor="lastName">LastName:</label>
+                <input
+                    type="text"
+                    name="lastName"
+                    id="lastName"
+                    value={user?.lastName}
+                    onChange={handleUserChange}
                 />
                 <label htmlFor="password">Password:</label>
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    defaultValue={token}
-                    onChange={saveFormData}
-                    required
-                />
+                <div className="password">
+                    <input
+                        type={type}
+                        name="password"
+                        placeholder={password}
+                        id="password"
+                        defaultValue={password}
+                        onChange={e => setPassword(e.target.value)}
+                        autoComplete="current-password"
+                    />
+                    <span className="password" onClick={handleToggle}>
+                        <Icon className="password" icon={icon} size={25}/>
+                    </span>
+                </div>
                 <button type="submit">Save</button>
                 <button type="button" onClick={routChange}>Discard</button>
             </form>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  commonjsOptions: {
+    esmExternals: true,
+  },
   test: {
     globals: true,
     include: ['**/*.test.tsx'],

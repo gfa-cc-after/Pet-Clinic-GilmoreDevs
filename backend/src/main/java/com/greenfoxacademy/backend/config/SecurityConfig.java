909f6cfa307a09e5d963b51db81e6a94500ebc7f
markkovari <kovarimarkofficial@gmail.com>
2024-08-10 10:26:33 +0200

    chore: unfold asterisks

--- backend/src/main/java/com/greenfoxacademy/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/greenfoxacademy/backend/config/SecurityConfig.java
@@ -18,7 +18,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;

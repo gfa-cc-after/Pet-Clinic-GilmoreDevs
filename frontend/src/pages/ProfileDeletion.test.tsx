197eacdcd7639556d85596203fc21a1a51e2b3f4
markkovari <kovarimarkofficial@gmail.com>
2024-08-16 10:33:46 +0200

    test: profiledelete component

---
+++ frontend/src/pages/ProfileDeletion.test.tsx
@@ -0,0 +1,34 @@
+import { render } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { ProfileDeletion } from './ProfileDeletion';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+
+describe('ProfileDeletion', () => {
+  it('should render successfully', () => {
+    const component = render(<ProfileDeletion />, {
+      wrapper: BrowserRouter
+    });
+    expect(component).not.toBeNull();
+  });
+
+  it('should should have two buttons', () => {
+    const component = render(<ProfileDeletion />, {
+      wrapper: BrowserRouter
+    });
+
+    const buttons = component.getAllByRole('button');
+    expect(buttons).toHaveLength(2);
+  });
+
+  it('should navigate to / when the "Nope, take me back!" button is clicked', () => {
+    const component = render(<ProfileDeletion />, {
+      wrapper: BrowserRouter
+    });
+
+    const button = component.getByText('Nope, take me back!');
+    userEvent.click(button);
+
+    expect(window.location.pathname).toBe('/');
+  });
+});

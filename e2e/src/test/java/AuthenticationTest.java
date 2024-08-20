bddebc35927aed503c9b0423ab9a6dee0eed68ac
markkovari <kovarimarkofficial@gmail.com>
2024-08-20 15:36:14 +0200

    refactor: use junit test runner and rename testclass

--- e2e/src/test/java/AuthenticationTest.java
+++ e2e/src/test/java/AuthenticationTest.java
@@ -1,22 +1,18 @@
 import com.github.javafaker.Faker;
 import com.microsoft.playwright.*;
-import com.microsoft.playwright.options.AriaRole;
 import model.RegisterUser;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.testcontainers.containers.DockerComposeContainer;
-import org.testcontainers.shaded.org.checkerframework.checker.regex.qual.Regex;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import pages.LoginPage;
 import pages.RegisterPage;
 import util.BrowserInstance;
 
-import java.io.File;
-import java.util.regex.Pattern;
-
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 
-public class Main {
+public class AuthenticationTest {
   private static final String HOME_URL = "http://localhost:5173";
   private static final Faker FAKER = new Faker();
 
@@ -28,28 +24,34 @@ public class Main {
     LAUNCH_OPTIONS.setSlowMo(1000);
   }
 
-  public static void main(String[] args) {
-    try (BrowserInstance instance = new BrowserInstance(LAUNCH_OPTIONS); Page page = instance.getPage()) {
-      registerSuccessful(page);
-      loginSuccessful(page);
-    } catch (Exception e) {
-      System.err.println(e.getLocalizedMessage());
-    }
+  private static BrowserInstance browserInstance;
+  private static Page page;
+
+  @BeforeAll
+  public static void setup() {
+    browserInstance = new BrowserInstance(LAUNCH_OPTIONS);
+    page = browserInstance.getPage();
   }
 
-  public static void registerSuccessful(Page page) {
+  @AfterAll
+  public static void tearDown() throws Exception {
+    page.close();
+    browserInstance.close();
+  }
+
+  @DisplayName("When user registers they can login too")
+  @Test
+  public void registerSuccessful() {
     page.navigate(HOME_URL + "/register");
     RegisterPage registerPage = new RegisterPage(page);
     registerPage.fillWithUser(USER);
     registerPage.clickRegister();
     registerPage.assertSuccessMessageVisible();
-  }
-
-  public static void loginSuccessful(Page page) {
     page.navigate(HOME_URL + "/login");
     LoginPage loginPage = new LoginPage(page);
     loginPage.fillWithUser(USER);
     loginPage.clickLogin();
     loginPage.assertSuccessMessageVisible();
   }
+
 }

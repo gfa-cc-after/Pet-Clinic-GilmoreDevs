b69c840048cd359ca1e3bce1c33faeec926e4fb0
Anna Ugrai <ugraianna@gmail.com>
2024-09-05 20:29:30 +0200

    chore: configure TTL with Redis

---
+++ backend/src/main/java/com/greenfoxacademy/backend/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.greenfoxacademy.backend.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+/**
+ * RedisConfig is a configuration class that defines the cache management strategy
+ * for Redis in the Spring Boot application. It configures the Redis Cache Manager
+ * and sets key serialization and expiration policies.
+ */
+
+@Configuration
+public class RedisConfig {
+
+  @Bean
+  public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+    RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(5))  // Set TTL to 5 minutes
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
+
+    return RedisCacheManager.builder(redisConnectionFactory)
+            .cacheDefaults(cacheConfig)
+            .build();
+  }
+}

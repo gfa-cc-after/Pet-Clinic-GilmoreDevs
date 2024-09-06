7d302d040831b3a82388d8018898fd79f22cd3b9
markkovari <kovarimarkofficial@gmail.com>
2024-09-06 10:34:44 +0200

    chore: make redis connection conditional

--- backend/src/main/java/com/greenfoxacademy/backend/config/RedisConfig.java
+++ backend/src/main/java/com/greenfoxacademy/backend/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.greenfoxacademy.backend.config;
 
+import java.time.Duration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,23 +11,28 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.time.Duration;
-
 /**
  * RedisConfig is a configuration class that defines the cache management strategy
  * for Redis in the Spring Boot application. It configures the Redis Cache Manager
  * and sets key serialization and expiration policies.
  */
-
 @Configuration
+@ConditionalOnProperty(name = "spring.cache.type", havingValue = "redis")
 public class RedisConfig {
 
+  /**
+   * The cacheManager method creates a RedisCacheManager bean that is used to manage.
+   */
   @Bean
   public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
     RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
             .entryTtl(Duration.ofMinutes(3)) // Set TTL to 5 minutes
             .disableCachingNullValues()
-            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
+            .serializeKeysWith(
+                    RedisSerializationContext
+                            .SerializationPair
+                            .fromSerializer(new StringRedisSerializer())
+            );
 
     return RedisCacheManager.builder(redisConnectionFactory)
             .cacheDefaults(cacheConfig)

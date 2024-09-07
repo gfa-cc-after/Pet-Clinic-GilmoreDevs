710241851eebec3ae015a04008f3d221793bc5c0
markkovari <kovarimarkofficial@gmail.com>
2024-09-07 09:46:55 +0200

    chore: add FeatureFlags config

---
+++ backend/src/main/java/com/greenfoxacademy/backend/config/FeatureFlags.java
@@ -0,0 +1,10 @@
+package com.greenfoxacademy.backend.config;
+
+import lombok.Getter;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class FeatureFlags {
+
+}
